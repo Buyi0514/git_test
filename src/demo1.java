e3ca62fd3dcf108531f6ef4cbdd0a7b241031faa
Lizen <941794689@qq.com>
2022-06-13 23:45:05 +0800

    hot-fix commit1 2022.6.13

--- src/demo1.java
+++ src/demo1.java
@@ -3,5 +3,6 @@ public class demo1 {
     public static void main(String[] args) {
         System.out.println("hello git !");
         System.out.println("hello lnz !");
+        System.out.println("hello hot-fix !");
     }
 }
